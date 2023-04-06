c74d454693f28bcabd73c0dbe2583f9bd6582215
Momin Ahmad <mominahmad93@gmail.com>
2023-04-06 13:24:36 -0400

    deployed to mainnet

--- deploy_factory.sh
+++ deploy_factory.sh
@@ -8,16 +8,16 @@ brew install jq
 
 source .env
 
-ModuleFactory=$(forge create --rpc-url $GOERLI_RPC_URL --private-key $GOERLI_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY contracts/modules/factories/ModuleFactory.sol:ModuleFactory --verify --json)
+ModuleFactory=$(forge create --rpc-url $MAINNET_RPC_URL --private-key $MAINNET_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY contracts/modules/factories/ModuleFactory.sol:ModuleFactory --verify --json)
 ModuleFactoryAddress=$(echo "$ModuleFactory" | jq -r '.deployedTo')
 ModuleFactoryEntry="contracts\/factory\/ModuleFactory.sol:ModuleFactory:$ModuleFactoryAddress"
 echo $ModuleFactoryAddress
 
 # Constructor Arguments: ModuleFactory Address, Arbiter Address, Oracle Address, Swap Target Address
 
-LineFactory=$(forge create --rpc-url $GOERLI_RPC_URL \
---constructor-args $ModuleFactoryAddress 0x895A8900437ba52A7C1450b09CD05C2Ba8A0EBE5 0x93E01461b2B02Fe872C1C5CceAd3E334BFA3C0De 0x47B005bC1AD130D6a61c2d21047Ee84e03e5Aa8f \
---private-key $GOERLI_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY \
+LineFactory=$(forge create --rpc-url $MAINNET_RPC_URL \
+--constructor-args $ModuleFactoryAddress 0xeb0566b1EF38B95da2ed631eBB8114f3ac7b9a8a 0x5a4AAF300473eaF8A9763318e7F30FA8a3f5Dd48 0xdef1c0ded9bec7f1a1670819833240f027b25eff \
+--private-key $MAINNET_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY \
 contracts/modules/factories/LineFactory.sol:LineFactory --verify --json)]
 LineFactoryAddress=$(echo "$LineFactory" | jq -r '.deployedTo')
 echo $LineFactoryAddress
