12a209a144010952e0e87ead60e46309f7f8291b
Momin Ahmad <mominahmad93@gmail.com>
2023-04-09 13:49:42 -0500

    added some stuff

--- contracts/simulations/IndexRe7Sim.t.sol
+++ contracts/simulations/IndexRe7Sim.t.sol
@@ -21,7 +21,7 @@ interface IWeth {
     function deposit() external payable;
 }
 
-interface dsETH {} //  TODO - define this
+interface IdsETH {} //  TODO - define this
 
 contract IndexRe7Sim is Test {
     bytes32 constant DEFAULT_ADMIN_ROLE = 0x00;
@@ -76,10 +76,25 @@ contract IndexRe7Sim is Test {
 
         vm.deal(borrowerAddress, 100 ether);
         vm.deal(arbiterAddress, 100 ether);
+
+        vm.startPrank(borrowerAddress);
+        securedLine = _deployLoCWithConfig();
+        vm.stopPrank();
+
     }
 
     ///////////////////////////////////////////////////////
-    //                      T E S T S                    //
+    //             S C E N A R I O   T E S T             //
+    ///////////////////////////////////////////////////////
+
+    function test_index_re7_simulation() public {
+        
+    }
+
+
+
+    ///////////////////////////////////////////////////////
+    //                U N I T   T E S T S                //
     ///////////////////////////////////////////////////////
 
     // select a specific fork
@@ -95,31 +110,58 @@ contract IndexRe7Sim is Test {
     }
 
     function test_borrower_can_deploy_LoC() public {
-        vm.startPrank(borrowerAddress);
-        // ILineFactory.CoreLineParams memory coreParams = ILineFactory.CoreLineParams({
-        //     borrower: borrowerAddress,
-        //     ttl: ttl, // time to live
-        //     cratio: minCRatio, // uint32(creditRatio),
-        //     revenueSplit: revenueSplit // uint8(revenueSplit) - 100% to spigot
-        // });
+        // vm.startPrank(borrowerAddress);
+        // securedLine = _deployLoCWithConfig();
 
-        // securedLine = lineFactory.deploySecuredLineWithConfig(coreParams);
-        securedLine = _deployLoCWithConfig();
         assertEq(borrowerAddress, ILineOfCredit(address(securedLine)).borrower());
         assertEq(arbiterAddress, ILineOfCredit(address(securedLine)).arbiter());
+        
         // assertEq(ttl, ILineOfCredit(address(securedLine)).arbiter()); // TODO: check ttl
         // assertEq(mincRatio, ILineOfCredit(address(securedLine)).arbiter()); // TODO: check minCRatio
     }
 
     function test_arbiter_enables_stablecoin_collateral() public {
-        vm.startPrank(borrowerAddress);
-        securedLine = _deployLoCWithConfig();
-        vm.stopPrank();
+        // vm.startPrank(borrowerAddress);
+        // securedLine = _deployLoCWithConfig();
+        // vm.stopPrank();
 
         vm.startPrank(arbiterAddress);
         address escrowAddress = address(ISecuredLine(address(securedLine)).escrow());
         bool collateralEnabled = IEscrow(address(escrowAddress)).enableCollateral(DAI);
         assertEq(true, collateralEnabled);
+    }
+
+
+    function test_borrower_can_draw_on_credit() public {
+
+    }
+
+    function test_borrower_can_deposit_and_repay_debt() public {
+
+    }
+
+    function test_claim_spigot_revenue() public {
+
+    }
+
+    function test_arbiter_can_claim_and_trade() public {
+
+    }
+
+    function test_arbiter_can_claim_and_repay() public {
+
+    }
+
+    function test_interest_accrues_correctly() public {
+        // borrower deploys line
+
+        // borrower and lender agree to terms of position
+
+        // borrower draws on credit
+
+        // warp time
+
+        // check interest accrued
     }
 
     ///////////////////////////////////////////////////////
@@ -137,94 +179,79 @@ contract IndexRe7Sim is Test {
         securedLine = lineFactory.deploySecuredLineWithConfig(coreParams);
         return securedLine;
     }
-    // deploy the migration contract
-
-    // function _deployMigration() internal returns (IdleMigration migration) {
-    //     migration = new IdleMigration(
-    //         address(lineFactory),   // line factory
-    //         90 days                 // ttl
-    //     );
-    // }
 
 
-    // function _operatorCallDeposit(address _spigot) internal {
-    //     uint256 _depositTokensLength = IFeeCollector(idleFeeCollector).getNumTokensInDepositList();
-    //     bool[] memory _tokensEnabled = new bool[](_depositTokensLength);
-    //     uint256[] memory _minTokensOut = new uint256[](_depositTokensLength);
+    function _addSpigot(address _lineOfCredit) internal returns (bool){
 
-    //     // we'll skip the swapping and just send the Weth directly to the contract,
-    //     // so all deposit tokens can be disabled
-    //     for (uint256 i; i < _tokensEnabled.length;) {
-    //         _tokensEnabled[i] = false;
-    //         unchecked {
-    //             ++i;
-    //         }
-    //     }
+    }
 
-    //     bytes memory data = abi.encodeWithSelector(IFeeCollector.deposit.selector, _tokensEnabled, _minTokensOut, 0);
+    function _borrowerDrawsOnCredit(bytes32 id, uint256 amount) internal returns (bool) {
 
-    //     assertEq(IFeeCollector.deposit.selector, bytes4(data), "deposit selector should match first 4 bytes of data");
+    }
 
-    //     require(ISpigot(_spigot).isWhitelisted(bytes4(data)), "Not Whitelisted");
+    function _depoistAndRepay(uint256 amount) internal {
 
-    //     // test the function that was whitelisted in the migration contract
-    //     vm.startPrank(idleTreasuryLeagueMultiSig);
-    //     assertEq(idleTreasuryLeagueMultiSig, ISpigot(_spigot).operator(), "treasury multisig should be operator");
-    //     ISpigot(_spigot).operate(idleFeeCollector, data);
-    //     vm.stopPrank();
-    // }
+    }
 
-    // function _simulateRevenueGeneration(uint256 amt) internal returns (uint256 revenue) {
-    //     vm.deal(idleFeeCollector, amt + 0.5 ether); // add a bit to cover gas
+    function _depositAndClose() internal {
 
-    //     vm.prank(idleFeeCollector);
-    //     revenue = amt;
-    //     IWeth(WETH).deposit{value: revenue}();
+    }
 
-    //     assertEq(IERC20(WETH).balanceOf(idleFeeCollector), revenue, "fee collector balance should match revenue");
-    // }
+    function _lenderWithdraws(bytes32 id, uint256 amount) internal {
 
-    // /// @dev    Because they claim function is not set in the spigot, this will be a push payment only
-    // /// @dev    We need to call `deposit()` manually before claiming revenue, or there will be no revenue
-    // ///         to claim (because calling `deposit()` distribute revenue to beneficiaires,of which the spigot is one)
-    // function _claimRevenueOnBehalfOfSpigot(address _spigot, uint256 _expectedRevenue) internal {
-    //     bytes memory data = abi.encodePacked("");
-    //     ISpigot(_spigot).claimRevenue(idleFeeCollector, WETH, data);
-    //     assertEq(_expectedRevenue, IERC20(WETH).balanceOf(_spigot), "balance of spigot should match expected revenue");
-    // }
+    }
+
+    function _simulateRevenueGeneration(uint256 amt) internal returns (uint256 revenue) {
+        vm.deal(revenueContractAddress, amt + 0.5 ether); // add a bit to cover gas
+
+        vm.prank(revenueContractAddress);
+        revenue = amt;
+        IWeth(WETH).deposit{value: revenue}();
+
+        assertEq(IERC20(WETH).balanceOf(revenueContractAddress), revenue, "fee collector balance should match revenue");
+    }
+
+    /// @dev    Because they claim function is not set in the spigot, this will be a push payment only
+    /// @dev    We need to call `deposit()` manually before claiming revenue, or there will be no revenue
+    ///         to claim (because calling `deposit()` distribute revenue to beneficiaires,of which the spigot is one)
+    function _claimRevenueOnBehalfOfSpigot(address _spigot, uint256 _expectedRevenue) internal {
+        bytes memory data = abi.encodePacked("");
+        ISpigot(_spigot).claimRevenue(revenueContractAddress, WETH, data);
+        assertEq(_expectedRevenue, IERC20(WETH).balanceOf(_spigot), "balance of spigot should match expected revenue");
+    }
 
     
-    // // fund a loan as a lender
-    // function _lenderFundLoan(address _lineOfCredit) internal returns (bytes32 id) {
-    //     assertEq(vm.activeFork(), ethMainnetFork, "mainnet fork is not active");
+    // fund a loan as a lender
+    function _lenderFundLoan(address _lineOfCredit) internal returns (bytes32 id) {
+        assertEq(vm.activeFork(), ethMainnetFork, "mainnet fork is not active");
 
-    //     // vm.roll(block.number + 5000);
+        // vm.roll(block.number + 5000);
 
-    //     vm.startPrank(idleTreasuryLeagueMultiSig);
-    //     ILineOfCredit(_lineOfCredit).addCredit(
-    //         1000, // drate
-    //         1000, // frate
-    //         loanSizeInDai, // amount
-    //         DAI, // token
-    //         daiWhale // lender
-    //     );
-    //     vm.stopPrank();
+        vm.startPrank(borrowerAddress);
+        ILineOfCredit(_lineOfCredit).addCredit(
+            dRate, // drate
+            fRate, // frate
+            loanSizeInWETH, // amount
+            WETH, // token
+            lenderAddress // lender
+        );
+        vm.stopPrank();
 
-    //     vm.startPrank(daiWhale);
-    //     IERC20(DAI).approve(_lineOfCredit, loanSizeInDai);
-    //     id = ILineOfCredit(_lineOfCredit).addCredit(
-    //         1000, // drate
-    //         1000, // frate
-    //         loanSizeInDai, // amount
-    //         DAI, // token
-    //         daiWhale // lender
-    //     );
-    //     vm.stopPrank();
+        vm.startPrank(lenderAddress);
+        IERC20(DAI).approve(_lineOfCredit, loanSizeInWETH);
+        id = ILineOfCredit(_lineOfCredit).addCredit(
+            dRate, // drate
+            fRate, // frate
+            loanSizeInWETH, // amount
+            WETH, // token
+            lenderAddress // lender
+        );
+        vm.stopPrank();
 
-    //     assertEq(IERC20(DAI).balanceOf(address(_lineOfCredit)), loanSizeInDai, "LoC balance doesn't match");
+        assertEq(IERC20(DAI).balanceOf(address(_lineOfCredit)), loanSizeInWETH, "LoC balance doesn't match");
 
-    //     emit log_named_bytes32("credit id", id);
-    // }
+        emit log_named_bytes32("credit id", id);
+    }
 
 
 
