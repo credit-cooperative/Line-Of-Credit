6b3f71650d56294389cdb2d55345a905dd71ce29
Thomas Hepner <hepner.thomas@gmail.com>
2023-07-17 17:45:59 -0700

    feat: update simple revenue contract with new functionality for testing spigot's operate function in the frontend.

--- contracts/mock/SimpleRevenueContract.sol
+++ contracts/mock/SimpleRevenueContract.sol
@@ -4,13 +4,19 @@
  pragma solidity ^0.8.16;
 import {Denominations} from "chainlink/Denominations.sol";
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import { MutualUpgrade } from "./MutualUpgrade.sol";
 
 contract SimpleRevenueContract {
     address owner;
+    address manager;
+    address methodologist;
     IERC20 revenueToken;
+    mapping (address => uint256) public nonce;
 
     constructor(address _owner, address token) {
         owner = _owner;
+        manager = _owner;
+        methodologist = _owner;
         revenueToken = IERC20(token);
     }
 
@@ -48,10 +54,35 @@ contract SimpleRevenueContract {
         return owner;
     }
 
+    function getManager() external view returns (address) {
+        return manager;
+    }
+
+
+    function getMethodologist() external view returns (address) {
+        return manager;
+    }
+
     function transferOwnership(address newOwner) external returns (bool) {
         require(msg.sender == owner, "Revenue: Only owner can transfer");
         owner = newOwner;
         return true;
+    }
+
+    function setMethodologist(address newMethodologist) external {
+        require(msg.sender == methodologist, "Only methodologist can call");
+        methodologist = newMethodologist;
+    }
+
+    function setManager(address newManager) external mutualUpgrade(manager, methodologist) {
+        require(_newManager != address(0), "Zero address not valid");
+        manager = newManager;
+        return true;
+    }
+
+    function updateNonce(address _address, uint256 num) external {
+        require(msg.sender == manager, "Only manager can call");
+        nonce[_address] += num;
     }
 
     receive() external payable {}
