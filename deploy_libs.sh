3958c9cf809bd715e704beee1c4913c284ef7a15
Momin Ahmad <mominahmad93@gmail.com>
2023-07-24 18:09:31 -0500

    prepped scripts and toml for polygon deployment

--- deploy_libs.sh
+++ deploy_libs.sh
@@ -10,56 +10,56 @@ brew install jq
 
 source .env
 
-LineLib=$(forge create --rpc-url $GOERLI_RPC_URL --private-key $GOERLI_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY  --optimzer-runs 20000 contracts/utils/LineLib.sol:LineLib --json --verify)
+LineLib=$(forge create --rpc-url $POLYGON_RPC_URL --private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY  --optimzer-runs 20000 contracts/utils/LineLib.sol:LineLib --json --verify)
 LineLibAddress=$(echo "$LineLib" | jq -r '.deployedTo')
 LineLibEntry="contracts\/utils\/LineLib.sol:LineLib:$LineLibAddress"
 
-sed -i '' '/\[profile\.goerli\]/,/^\[/s/^libraries = \[.*\]/libraries = \["'$LineLibEntry'"\]/' foundry.toml
+sed -i '' '/\[profile\.polygon\]/,/^\[/s/^libraries = \[.*\]/libraries = \["'$LineLibEntry'"\]/' foundry.toml
 
 source .env
 
-CreditLib=$(forge create --rpc-url $GOERLI_RPC_URL --private-key $GOERLI_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 20000 contracts/utils/CreditLib.sol:CreditLib --verify --json)
+CreditLib=$(forge create --rpc-url $POLYGON_RPC_URL --private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 20000 contracts/utils/CreditLib.sol:CreditLib --verify --json)
 CreditLibAddress=$(echo "$CreditLib" | jq -r '.deployedTo')
 CreditLibEntry="contracts\/utils\/CreditLib.sol:CreditLib:$CreditLibAddress"
 
-sed -i '' '/\[profile\.goerli\]/,/^\[/s/^libraries = \["'$LineLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'"\]/' foundry.toml
+sed -i '' '/\[profile\.polygon\]/,/^\[/s/^libraries = \["'$LineLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'"\]/' foundry.toml
 
 source .env
 
-CreditListLib=$(forge create --rpc-url $GOERLI_RPC_URL --private-key $GOERLI_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 20000 contracts/utils/CreditListLib.sol:CreditListLib --verify --json)
+CreditListLib=$(forge create --rpc-url $POLYGON_RPC_URL --private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 20000 contracts/utils/CreditListLib.sol:CreditListLib --verify --json)
 CreditListLibAddress=$(echo "$CreditListLib" | jq -r '.deployedTo')
 CreditListLibEntry="contracts\/utils\/CreditListLib.sol:CreditListLib:$CreditListLibAddress"
 
-sed -i '' '/\[profile\.goerli\]/,/^\[/s/^libraries = \["'$LineLibEntry'","'$CreditLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'"\]/' foundry.toml
+sed -i '' '/\[profile\.polygon\]/,/^\[/s/^libraries = \["'$LineLibEntry'","'$CreditLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'"\]/' foundry.toml
 
 source .env
 
-SpigotLib=$(forge create --rpc-url $GOERLI_RPC_URL --private-key $GOERLI_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 20000 contracts/utils/SpigotLib.sol:SpigotLib --verify --json)
+SpigotLib=$(forge create --rpc-url $POLYGON_RPC_URL --private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 20000 contracts/utils/SpigotLib.sol:SpigotLib --verify --json)
 SpigotLibAddress=$(echo "$SpigotLib" | jq -r '.deployedTo')
 SpigotLibEntry="contracts\/utils\/SpigotLib.sol:SpigotLib:$SpigotLibAddress"
 
-sed -i '' '/\[profile\.goerli\]/,/^\[/s/^libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'"\]/' foundry.toml
+sed -i '' '/\[profile\.polygon\]/,/^\[/s/^libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'"\]/' foundry.toml
 
 source .env
 
-EscrowLib=$(forge create --rpc-url $GOERLI_RPC_URL --private-key $GOERLI_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 20000 contracts/utils/EscrowLib.sol:EscrowLib --verify --json)
+EscrowLib=$(forge create --rpc-url $POLYGON_RPC_URL --private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 20000 contracts/utils/EscrowLib.sol:EscrowLib --verify --json)
 EscrowLibAddress=$(echo "$EscrowLib" | jq -r '.deployedTo')
 EscrowLibEntry="contracts\/utils\/EscrowLib.sol:EscrowLib:$EscrowLibAddress"
 
-sed -i '' '/\[profile\.goerli\]/,/^\[/s/^libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'","'$EscrowLibEntry'"\]/' foundry.toml
+sed -i '' '/\[profile\.polygon\]/,/^\[/s/^libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'","'$EscrowLibEntry'"\]/' foundry.toml
 
 source .env
 
-SpigotedLineLib=$(forge create --rpc-url $GOERLI_RPC_URL --private-key $GOERLI_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 20000 contracts/utils/SpigotedLineLib.sol:SpigotedLineLib --verify --json)
+SpigotedLineLib=$(forge create --rpc-url $POLYGON_RPC_URL --private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 20000 contracts/utils/SpigotedLineLib.sol:SpigotedLineLib --verify --json)
 SpigotedLineLibAddress=$(echo "$SpigotedLineLib" | jq -r '.deployedTo')
 SpigotedLineLibEntry="contracts\/utils\/SpigotedLineLib.sol:SpigotedLineLib:$SpigotedLineLibAddress"
 
-sed -i '' '/\[profile\.goerli\]/,/^\[/s/^libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'","'$EscrowLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'","'$EscrowLibEntry'","'$SpigotedLineLibEntry'"\]/' foundry.toml
+sed -i '' '/\[profile\.polygon\]/,/^\[/s/^libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'","'$EscrowLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'","'$EscrowLibEntry'","'$SpigotedLineLibEntry'"\]/' foundry.toml
 
 source .env
 
-LineFactoryLib=$(forge create --rpc-url $GOERLI_RPC_URL --private-key $GOERLI_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 100 contracts/utils/LineFactoryLib.sol:LineFactoryLib --verify --json)
+LineFactoryLib=$(forge create --rpc-url $POLYGON_RPC_URL --private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY --optimzer-runs 100 contracts/utils/LineFactoryLib.sol:LineFactoryLib --verify --json)
 LineFactoryLibAddress=$(echo "$LineFactoryLib" | jq -r '.deployedTo')
 LineFactoryLibEntry="contracts\/utils\/LineFactoryLib.sol:LineFactoryLib:$LineFactoryLibAddress"
 
-sed -i '' '/\[profile\.goerli\]/,/^\[/s/^libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'","'$EscrowLibEntry'","'$SpigotedLineLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'","'$EscrowLibEntry'","'$SpigotedLineLibEntry'","'$LineFactoryLibEntry'"\]/' foundry.toml
+sed -i '' '/\[profile\.polygon\]/,/^\[/s/^libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'","'$EscrowLibEntry'","'$SpigotedLineLibEntry'"\]/libraries = \["'$LineLibEntry'","'$CreditLibEntry'","'$CreditListLibEntry'","'$SpigotLibEntry'","'$EscrowLibEntry'","'$SpigotedLineLibEntry'","'$LineFactoryLibEntry'"\]/' foundry.toml
