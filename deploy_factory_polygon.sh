b485be23215f62779a597351b014910d27644aa0
Momin Ahmad <mominahmad93@gmail.com>
2023-07-26 10:04:22 -0500

    different deploy scripts for different chains

--- deploy_factory_polygon.sh
+++ deploy_factory_polygon.sh
@@ -13,10 +13,10 @@ ModuleFactoryAddress=$(echo "$ModuleFactory" | jq -r '.deployedTo')
 ModuleFactoryEntry="contracts\/factory\/ModuleFactory.sol:ModuleFactory:$ModuleFactoryAddress"
 echo $ModuleFactoryAddress
 
-# Constructor Arguments: ModuleFactory Address, Arbiter Address, Oracle Address, Swap Target Address
+# Constructor Arguments: ModuleFactory Address, Arbiter Address, Oracle Address, Swap Target Address (same as Mainnet)
 
 LineFactory=$(forge create --rpc-url $GOERLI_RPC_URL \
---constructor-args $ModuleFactoryAddress 0x895A8900437ba52A7C1450b09CD05C2Ba8A0EBE5 0x93E01461b2B02Fe872C1C5CceAd3E334BFA3C0De 0x47B005bC1AD130D6a61c2d21047Ee84e03e5Aa8f \
+--constructor-args $ModuleFactoryAddress  0xFE002526dEc5B3e4b5134b75b20c065178323343 '',  0xdef1c0ded9bec7f1a1670819833240f027b25eff \
 --private-key $GOERLI_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY \
 contracts/modules/factories/LineFactory.sol:LineFactory --verify --json)]
 LineFactoryAddress=$(echo "$LineFactory" | jq -r '.deployedTo')
