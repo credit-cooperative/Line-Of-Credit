3f76d13701c7e1ca6c1e25fee8ad2d685060dbfc
Momin Ahmad <mominahmad93@gmail.com>
2023-07-26 19:11:25 -0500

    deploy LoC

--- deploy_line_of_credit_polygon.sh
+++ deploy_line_of_credit_polygon.sh
@@ -11,24 +11,24 @@ source .env
 
 ### constructor arguments: minCRatio, oracle address, owner, borrower
 Escrow=$(forge create --rpc-url $POLYGON_RPC_URL \
---constructor-args 0 0x93E01461b2B02Fe872C1C5CceAd3E334BFA3C0De 0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe 0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe \
+--constructor-args 0 0x22acC1a2Db2d812d5443DD26F22F4b7b029f2b08 0xf44B95991CaDD73ed769454A03b3820997f00873 0x9c59770DEf99d56B33208AE3124ef71865cFA6e9 \
 --private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $POLYGON_ETHERSCAN_API_KEY contracts/modules/escrow/Escrow.sol:Escrow --verify --json)
 EscrowAddress=$(echo "$Escrow" | jq -r '.deployedTo')
 echo $EscrowAddress
 
 
 ### constructor arguments: owner, operator
-Spigot=$(forge create --rpc-url $GOERLI_RPC_URL \
---constructor-args 0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe 0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe \
+Spigot=$(forge create --rpc-url $POLYGON_RPC_URL \
+--constructor-args 0xf44B95991CaDD73ed769454A03b3820997f00873 0x7b4d2057d294879467a6f83893b55eedc72bf5a6 \
 --private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $POLYGON_ETHERSCAN_API_KEY contracts/modules/spigot/Spigot.sol:Spigot --verify --json)
 SpigotAddress=$(echo "$Spigot" | jq -r '.deployedTo')
 echo $SpigotAddress
 
 
 ### constructor arguments: oracle, arbiter, borrower, swapTarget, spigot, escrow, ttl (3 days), defaultSplit
-SecuredLine=$(forge create --rpc-url $GOERLI_RPC_URL \
---constructor-args 0x93E01461b2B02Fe872C1C5CceAd3E334BFA3C0De 0x895A8900437ba52A7C1450b09CD05C2Ba8A0EBE5 0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe 0x47B005bC1AD130D6a61c2d21047Ee84e03e5Aa8f $SpigotAddress $EscrowAddress 259200 50 \
---private-key $GOERLI_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY contracts/modules/credit/SecuredLine.sol:SecuredLine --verify --json)
+SecuredLine=$(forge create --rpc-url $POLYGON_RPC_URL \
+--constructor-args 0x22acC1a2Db2d812d5443DD26F22F4b7b029f2b08 0xFE002526dEc5B3e4b5134b75b20c065178323343 0x9c59770DEf99d56B33208AE3124ef71865cFA6e9 0xdef1c0ded9bec7f1a1670819833240f027b25eff $SpigotAddress $EscrowAddress 259200 50 \
+--private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $POLYGON_ETHERSCAN_API_KEY contracts/modules/credit/SecuredLine.sol:SecuredLine --verify --json)
 SecuredLineAddress=$(echo "$SecuredLine" | jq -r '.deployedTo')
 echo $SecuredLineAddress
 
