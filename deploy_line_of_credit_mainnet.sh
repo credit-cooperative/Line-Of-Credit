a7f4924f07c100cc13ebe6c89dc7d824a35dc05a
Thomas Hepner <hepner.thomas@gmail.com>
2023-09-07 14:07:47 -0700

    fix: modify deployment scripts for mock rain contract testing.

--- deploy_line_of_credit_mainnet.sh
+++ deploy_line_of_credit_mainnet.sh
@@ -11,7 +11,7 @@ source .env
 
 ### constructor arguments: minCRatio, oracle address, owner, borrower
 Escrow=$(forge create --rpc-url $MAINNET_RPC_URL \
---constructor-args 5000 0x93E01461b2B02Fe872C1C5CceAd3E334BFA3C0De 0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe 0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe \
+--constructor-args 0 0x5a4AAF300473eaF8A9763318e7F30FA8a3f5Dd48 0x06dae7Ba3958EF288adB0B9b3732eC204E48BC47 0x06dae7Ba3958EF288adB0B9b3732eC204E48BC47 \
 --private-key $MAINNET_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY contracts/modules/escrow/Escrow.sol:Escrow --verify --json)
 EscrowAddress=$(echo "$Escrow" | jq -r '.deployedTo')
 echo $EscrowAddress
@@ -19,7 +19,7 @@ echo $EscrowAddress
 
 ### constructor arguments: owner, operator
 Spigot=$(forge create --rpc-url $MAINNET_RPC_URL \
---constructor-args 0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe 0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe \
+--constructor-args 0x06dae7Ba3958EF288adB0B9b3732eC204E48BC47 0x06dae7Ba3958EF288adB0B9b3732eC204E48BC47 \
 --private-key $MAINNET_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY contracts/modules/spigot/Spigot.sol:Spigot --verify --json)
 SpigotAddress=$(echo "$Spigot" | jq -r '.deployedTo')
 echo $SpigotAddress
@@ -27,7 +27,7 @@ echo $SpigotAddress
 
 ### constructor arguments: oracle, arbiter, borrower, swapTarget, spigot, escrow, ttl (3 days), defaultSplit
 SecuredLine=$(forge create --rpc-url $MAINNET_RPC_URL \
---constructor-args 0x93E01461b2B02Fe872C1C5CceAd3E334BFA3C0De 0x895A8900437ba52A7C1450b09CD05C2Ba8A0EBE5 0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe 0x47B005bC1AD130D6a61c2d21047Ee84e03e5Aa8f $SpigotAddress $EscrowAddress 259200 50 \
+--constructor-args 0x5a4AAF300473eaF8A9763318e7F30FA8a3f5Dd48 0xeb0566b1EF38B95da2ed631eBB8114f3ac7b9a8a 0x06dae7Ba3958EF288adB0B9b3732eC204E48BC47 0xDef1C0ded9bec7F1a1670819833240f027b25EfF $SpigotAddress $EscrowAddress 259200 100 \
 --private-key $MAINNET_PRIVATE_KEY --etherscan-api-key $MAINNET_ETHERSCAN_API_KEY contracts/modules/credit/SecuredLine.sol:SecuredLine --verify --json)
 SecuredLineAddress=$(echo "$SecuredLine" | jq -r '.deployedTo')
 echo $SecuredLineAddress
--- deploy_test_infra_rain_polygon.sh
+++ deploy_test_infra_rain_polygon.sh
@@ -27,7 +27,7 @@ contracts/mock/Rain/MockRainBeacon.sol:MockRainBeacon  --verify
 ### Collateral ###
 
 # Constructor Args: owner, factory
-forge create --rpc-url $POLYGON_RPC_URL --constructor-args 0x97fCbc96ed23e4E9F0714008C8f137D57B4d6C97 0x4955634FFDac4c88BfBA71D7093FB1b9259a9647 \
+forge create --rpc-url $POLYGON_RPC_URL --constructor-args 0x06dae7Ba3958EF288adB0B9b3732eC204E48BC47 0xb2a475abeeec287fdfda37f6977803989e388f53 \
 --private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $POLYGON_ETHERSCAN_API_KEY \
 contracts/mock/Rain/MockRainCollateral.sol:MockRainCollateral --verify
 
