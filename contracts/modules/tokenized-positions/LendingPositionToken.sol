1b50e8e9ce9eaf3918fa82906b83510c010b4a58
Momin Ahmad <mominahmad93@gmail.com>
2023-12-14 13:52:33 -0600

    seperate func for cratio due to state change

--- contracts/modules/tokenized-positions/LendingPositionToken.sol
+++ contracts/modules/tokenized-positions/LendingPositionToken.sol
@@ -9,25 +9,25 @@ import {ISpigotedLine} from "../../interfaces/ISpigotedLine.sol";
 import {IEscrowedLine} from "../../interfaces/IEscrowedLine.sol";
 import {IEscrow} from "../../interfaces/IEscrow.sol";
 
-// TODO: Add back IERC721Enumerable and functions or use https://docs.simplehash.com/reference/nfts-by-owners
+// TODO: Add back IERC721Enumerable and functions or use https://docs.simplehash.com/reference/nfts-by-owners to get owner of token
+
 contract LendingPositionToken is ERC721 {
     uint256 private _tokenIds;
     mapping(uint256 => address) private tokenToLine;
 
-    struct UnderlyingPositionInfo {
+    struct UnderlyingInfo {
         address line;
         bytes32 id;
         uint256 deposit;
         uint256 principal;
         uint256 interestAccrued;
         uint256 interestRepaid;
-    }
-
-    struct UnderlyingLineInfo {
         uint256 deadline;
         uint256 split;
-        uint256 cratio;
+        uint256 mincratio;
     }
+
+
     constructor() ERC721("LendingPositionToken", "LPT") {}
 
     function mint(address to, address line) public returns (uint256) {
@@ -38,48 +38,51 @@ contract LendingPositionToken is ERC721 {
         return newItemId;
     }
 
-    function getUnderlyingPositionInfo(uint256 tokenId)
+    function getUnderlyingInfo(uint256 tokenId)
         public
         view
-        returns (UnderlyingPositionInfo memory)
+        returns (UnderlyingInfo memory)
     {
         uint256 value = 0;
         address line = tokenToLine[tokenId];
 
         (ILineOfCredit.Credit memory credit, bytes32 id) = ILineOfCredit(line).getPositionFromTokenId(tokenId);
-
-        
         
         (uint128 dRate, uint128 fRate) = ILineOfCredit(line).getRates(id);
         uint256 deposit = credit.deposit;
         uint256 principal = credit.principal;
         uint256 interestAccrued = credit.interestAccrued;
         uint256 interestRepaid = credit.interestRepaid;
+        uint256 deadline = ILineOfCredit(line).getDeadline();
+        uint256 split = ISpigotedLine(line).getSplit();
 
+        address escrow = address(IEscrowedLine(line).escrow());
+        uint256 mincratio = IEscrow(escrow).minimumCollateralRatio();
 
         return
-            UnderlyingPositionInfo(
+            UnderlyingInfo(
                 line,
                 id,
                 deposit,
                 principal,
                 interestAccrued,
-                interestRepaid
+                interestRepaid,
+                deadline,
+                split,
+                mincratio
             );
     }
 
-    // TODO: (current) need to have a sepearate function due to cratio changing state. 
-    //(future) May split out cratio to seperaate func and add other view details to above func
-
-    function getUnderlyingLineInfo(uint256 tokenId)
+    
+    // NOTE: seperate func bc this function changes state
+    function getCRatio(uint256 tokenId)
         public
-        returns (UnderlyingLineInfo memory)
+        returns (uint256 )
     {
         address line = tokenToLine[tokenId];
-        uint256 deadline = ILineOfCredit(line).getDeadline();
-        uint256 split = ISpigotedLine(line).getSplit();
+        
         address escrow = address(IEscrowedLine(line).escrow());
         uint256 cratio = IEscrow(escrow).getCollateralRatio();
-        return UnderlyingLineInfo(deadline, split, cratio);
+        return cratio;
     }
 }
