173e9f42402aebb829ef73920aa8b661eb5d65ff
Momin Ahmad <mominahmad93@gmail.com>
2023-12-27 18:17:19 -0600

    toml with latest polygon libs

--- scripts/deploy_factory_polygon.sh
+++ scripts/deploy_factory_polygon.sh
@@ -16,7 +16,7 @@ echo $ModuleFactoryAddress
 # Constructor Arguments: ModuleFactory Address, Arbiter Address, Oracle Address, Swap Target Address (same as Mainnet)
 
 LineFactory=$(forge create --rpc-url $POLYGON_RPC_URL \
---constructor-args 0x582Da20CE3Eb970467Ac68be1c9Ee0997BC8A7e9 0xFE002526dEc5B3e4b5134b75b20c065178323343 0xF1baA8242e3AAF65D4Eb030459854cddE209acb9 0xdef1c0ded9bec7f1a1670819833240f027b25eff \
+--constructor-args 0x4AE0962441B763981000A99306AB5016238B8F40 0xFE002526dEc5B3e4b5134b75b20c065178323343 0xF1baA8242e3AAF65D4Eb030459854cddE209acb9 0xdef1c0ded9bec7f1a1670819833240f027b25eff \
 --private-key $POLYGON_PRIVATE_KEY --etherscan-api-key $POLYGONSCAN_API_KEY \
 contracts/modules/factories/LineFactory.sol:LineFactory --verify --json)]
 LineFactoryAddress=$(echo "$LineFactory" | jq -r '.deployedTo')
