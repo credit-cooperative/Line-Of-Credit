4de8d353d61c81e6de57b7b0872e54cf38320b99
Momin Ahmad <mominahmad93@gmail.com>
2023-12-29 14:17:17 -0600

    deployed to arbitrum

--- scripts/deploy_factory_arbitrum.sh
+++ scripts/deploy_factory_arbitrum.sh
@@ -13,10 +13,15 @@ brew install jq
 # ModuleFactoryEntry="contracts\/factory\/ModuleFactory.sol:ModuleFactory:$ModuleFactoryAddress"
 # echo $ModuleFactoryAddress
 
+
+### DEPLOY Oracle ###
+
+Oracle=$(forge create --rpc-url $ARBITRUM_RPC_URL --private-key $ARBITRUM_PRIVATE_KEY --etherscan-api-key $ARBISCAN_API_KEY contracts/modules/oracle/ArbitrumOracle.sol:ArbitrumOracle --verify --json)
+
 # Constructor Arguments: ModuleFactory Address, Arbiter Address, Oracle Address, Swap Target Address (same as Mainnet)
 
 LineFactory=$(forge create --rpc-url $ARBITRUM_RPC_URL \
---constructor-args $ModuleFactoryAddress 0xFE002526dEc5B3e4b5134b75b20c065178323343 <'oracle address here'> 0xdef1c0ded9bec7f1a1670819833240f027b25eff \
+--constructor-args 0x0f436F62f3CE9D2F25231f1DAE71b77A4F8EeDf9 0xFE002526dEc5B3e4b5134b75b20c065178323343 0x47B005bC1AD130D6a61c2d21047Ee84e03e5Aa8f 0xdef1c0ded9bec7f1a1670819833240f027b25eff \
 --private-key $ARBITRUM_PRIVATE_KEY --etherscan-api-key $ARBISCAN_API_KEY \
 contracts/modules/factories/LineFactory.sol:LineFactory --verify --json)]
 LineFactoryAddress=$(echo "$LineFactory" | jq -r '.deployedTo')
