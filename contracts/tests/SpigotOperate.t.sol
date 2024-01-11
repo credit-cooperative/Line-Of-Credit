f770bbb86ef6749818882ab47298b905e19204a6
Momin Ahmad <mominahmad93@gmail.com>
2024-01-11 00:25:13 -0500

    setup, cant min

--- contracts/tests/SpigotOperate.t.sol
+++ contracts/tests/SpigotOperate.t.sol
@@ -10,8 +10,7 @@ import {ISpigot} from "../interfaces/ISpigot.sol";
 
 import {IERC721} from "openzeppelin/token/ERC721/IERC721.sol";
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
-
-
+import "forge-std/console.sol";
 
 interface Uni_V3_Manager {
     struct MintParams {
@@ -131,63 +130,56 @@ contract SpigotOperateTest is Test {
     ISpigot.Setting[] private s;
 
     // Spigot Controller access control vars
-    address private owner;
-    address private operator;
+    address owner = 0x9832FD4537F3143b5C2989734b11A54D4E85eEF6;
+    address operator = 0xf44B95991CaDD73ed769454A03b3820997f00873;
 
     uint256 MAX_INT =
         115792089237316195423570985008687907853269984665640564039457584007913129639935;
     uint256 mintAmount = 100000 ether;
 
-      // Fork Settings
-    uint256 constant FORK_BLOCK_NUMBER = 18_981_299; // Forking mainnet at block on 7/6/23 at 7 40 PM EST
+    // Fork Settings
+    uint256 constant FORK_BLOCK_NUMBER = 17_638_122; // Forking mainnet at block on 7/6/23 at 7 40 PM EST
     uint256 ethMainnetFork;
 
 
     function setUp() public {
         ethMainnetFork = vm.createFork(vm.envString("MAINNET_RPC_URL"), FORK_BLOCK_NUMBER);
         vm.selectFork(ethMainnetFork);
-
-        owner = address(11);
-        operator = address(10);
         
         spigot = new Spigot(owner, operator);
-
+        
         _mintAndApprove();
         _configureSpigot();
-        tokenId = _mintNFT();
-
-        // send uni v3 position to spigot
-        vm.startPrank(owner);
-        IERC721(UNI_V3_POSITION_MANAGER).transferFrom(owner, address(spigot), tokenId);
-
-        // check ownership of uni v3 position
-        assertEq(IERC721(UNI_V3_POSITION_MANAGER).ownerOf(tokenId), address(spigot));
-                
+             
     }
 
     function _mintAndApprove() public {
-
+        
         deal(WETH, owner, mintAmount);
         deal(USDC, operator, mintAmount);
-
+        
         vm.startPrank(owner);
         IERC20(WETH).approve(address(spigot), MAX_INT);
         IERC20(USDC).approve(address(spigot), MAX_INT);
+        
         IERC20(WETH).approve(UNI_V3_POSITION_MANAGER, MAX_INT);
         IERC20(USDC).approve(UNI_V3_POSITION_MANAGER, MAX_INT);
+        
         vm.stopPrank();
     }
 
     function _configureSpigot() public {
+        vm.startPrank(owner);
         settings = ISpigot.Setting(0, claimFunc, newOwnerFunc);
         spigot.addSpigot(UNI_V3_POSITION_MANAGER, settings);
-
+        
         spigot.updateWhitelistedFunction(decreaseLiquidityFunc, true);
         spigot.updateWhitelistedFunction(transferNFTFunc, true);
         spigot.updateWhitelistedFunction(claimFeesFunc, true);
 
         spigot.updateWhitelistedFunction(approveTransferFunc, false);
         spigot.updateWhitelistedFunction(transferNFTFunc, false);
+        vm.stopPrank();
     }
 
     function _mintNFT() public returns (uint256 tokenId) {
@@ -195,8 +187,8 @@ contract SpigotOperateTest is Test {
             token0: WETH,
             token1: USDC,
             fee: 3000, 
-            tickLower: -887272, // cant do this in solidity
-            tickUpper: -887272, // cant do this in solidity
+            tickLower: -887272, 
+            tickUpper: -897272,
             amount0Desired: 40000000000000000,
             amount1Desired: 100000000000000000000,
             amount0Min: 0,
@@ -252,7 +244,11 @@ contract SpigotOperateTest is Test {
         return abi.encodeWithSelector(approveTransferFunc, address(spigot), 0);
     }
 
-
+    function test_mint() public {
+        
+        tokenId = _mintNFT();
+        console.log("tokenId", tokenId);
+    }
 
     function test_operate_univ3() public {
 
