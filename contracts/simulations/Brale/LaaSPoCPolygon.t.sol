a5784400aa8ac3aa0ad1e660600cc64a9c3882c8
Momin Ahmad <mominahmad93@gmail.com>
2024-07-17 15:07:24 -0400

    init sim file

---
+++ contracts/simulations/Brale/LaaSPoCPolygon.t.sol
@@ -0,0 +1,140 @@
+pragma solidity ^0.8.9;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "openzeppelin/token/ERC20/extensions/IERC20Metadata.sol";
+import {LineFactory} from "../../modules/factories/LineFactory.sol";
+import {Spigot} from "../../modules/spigot/Spigot.sol";
+import {IPolygonOracle} from "../../interfaces/IPolygonOracle.sol";
+import {LineOfCredit} from "../../modules/credit/LineOfCredit.sol";
+import {SpigotedLine} from "../../modules/credit/SpigotedLine.sol";
+import {SecuredLine} from "../../modules/credit/SecuredLine.sol";
+import {ISpigotedLine} from "../../interfaces/ISpigotedLine.sol";
+import {IEscrow} from "../../interfaces/IEscrow.sol";
+import {Escrow} from "../../modules/escrow/Escrow.sol";
+import {ISpigot} from "../../interfaces/ISpigot.sol";
+import {Spigot} from "../../modules/spigot/Spigot.sol";
+import {ILineOfCredit} from "../../interfaces/ILineOfCredit.sol";
+import {ISecuredLine} from "../../interfaces/ISecuredLine.sol";
+import {ILineFactory} from "../../interfaces/ILineFactory.sol";
+import {SBCPriceFeedPolygon} from "../../modules/oracle/SBCPriceFeedPolygon.sol";
+
+
+contract BralePolygonSimple is Test {
+    IPolygonOracle public oracle;
+    IERC20 public usdc;
+    IERC20 public SBC;
+    ISecuredLine public securedLine;
+    ISpigotedLine public spigotedLine;
+    IEscrow public escrow;
+    ISpigot public spigot;
+    ISpigot.Setting private settings;
+    ILineFactory public lineFactory;
+    SecuredLine public line;
+
+    uint256 MAX_INT =
+        115792089237316195423570985008687907853269984665640564039457584007913129639935;
+
+    address constant lineFactoryAddress ; // todo 
+    uint256 ttl = 60 days;
+
+    address constant SBC ; // todo
+    address constant USDC ;// todo
+
+    address constant arbiter = 0xFE002526dEc5B3e4b5134b75b20c065178323343;
+    address constant borrower ; // todo
+    address constant lender ; // todo
+    address constant collateralMultisig ; // todo
+
+    SBCPriceFeedPolygon public priceFeed;
+    address public polygonOracle ; // todo
+    address public owner ; // todo
+
+    uint256 FORK_BLOCK_NUMBER ; // todo
+    uint256 polygonFork;
+    uint256 lentAmount = 100000 * 1e6; // 100k USDC
+    uint256 MARGIN_OF_ERROR = 0.001e18; //.1% margin of error (1e18 is 100%)
+    address lineAddress;
+
+    function setUp() public {
+        polygonFork = vm.createFork(vm.envString("POLYGON_RPC_URL"), FORK_BLOCK_NUMBER);
+        vm.selectFork(polygonFork);
+
+        oracle = IPolygonOracle(polygonOracle);
+
+        priceFeed = new SBCPriceFeedPolygon();
+        vm.startPrank(owner);
+        oracle.setPriceFeed(SBC, address(priceFeed)); // TODO: do this on arbitrum
+        vm.stopPrank();
+
+        deal(USDC, lender, lentAmount);
+        deal(SBC, borrower, lentAmount);
+
+        // NOTE: Leftover from before deploying the line on Arbitrum
+        ILineFactory.CoreLineParams memory coreParams = ILineFactory.CoreLineParams({
+            borrower: borrower,
+            ttl: ttl,
+            cratio: 2000,
+            revenueSplit: 0
+        });
+        
+        lineAddress = ILineFactory(lineFactoryAddress).deploySecuredLineWithConfig(coreParams);
+
+        line = SecuredLine(payable(lineAddress));
+
+        escrow = line.escrow();
+
+        _mintAndApprove();
+
+    }
+
+    function _mintAndApprove() public {
+        vm.startPrank(lender);
+        IERC20(USDC).approve(address(line), MAX_INT);
+        vm.stopPrank();
+
+        vm.startPrank(borrower);
+        IERC20(SBC).approve(address(escrow), MAX_INT);
+        vm.stopPrank();
+    }
+
+    function _addCredit() internal {
+        vm.startPrank(lender);
+        ILineOfCredit(lineAddress).addCredit(700, 700, lentAmount, USDC, lender);
+        vm.stopPrank();
+
+        vm.startPrank(borrower);
+        ILineOfCredit(lineAddress).addCredit(700, 700, lentAmount, USDC, lender);
+        vm.stopPrank();
+    }
+
+    // NOTE: This is a simple end to end test of D8X
+    function test_brale_credit_line() public {
+
+        // TODO borrower needs to add SBC to escrow
+
+        vm.startPrank(arbiter);
+        escrow.enableCollateral(SBC);
+        vm.stopPrank();
+
+        vm.startPrank(borrower);
+        escrow.addCollateral(20001 ether, SBC);
+        vm.stopPrank();
+
+        assertEq(IERC20(SBC).balanceOf(address(escrow)), 20001 ether);
+
+
+        _addCredit();
+
+        bytes32 id = line.ids(0);
+
+        vm.startPrank(borrower);
+        emit log_named_string('\n \u2713 borrower draws funds', '');
+        ILineOfCredit(lineAddress).borrow(id, lentAmount);
+        vm.stopPrank();
+
+        assertGt(escrow.getCollateralRatio(), escrow.minimumCollateralRatio());
+        assertEq(IERC20(USDC).balanceOf(borrower), lentAmount);
+    }
+}
