183e91283fbce86eb07ef7bf58c5a2eb863a7fdf
Momin Ahmad <mominahmad93@gmail.com>
2024-07-18 01:04:33 -0400

    .

--- contracts/modules/oracle/SBCPriceFeedPolygon.sol
+++ contracts/modules/oracle/SBCPriceFeedPolygon.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.16;
 import {IERC20Metadata} from "openzeppelin/token/ERC20/extensions/IERC20Metadata.sol";
 
 // Price feed for SBC Polygon
- contract stUSDPriceFeedArbitrum {
+ contract SBCPriceFeedPolygon {
 
     address SBC = 0xfdcC3dd6671eaB0709A4C0f3F53De9a333d80798; // polygon
     constructor () {}
@@ -14,10 +14,11 @@ import {IERC20Metadata} from "openzeppelin/token/ERC20/extensions/IERC20Metadata
         uint256 startedAt, 
         uint256 updatedAt, 
         uint80 answeredInRound) {
-        return (0, 1 * IERC20Metadata(SBC).decimals(), 0, block.timestamp, 0);
+        uint256 decimals = uint256(IERC20Metadata(SBC).decimals());
+        return (0, int256(1 * 10 ** decimals), 0, block.timestamp, 0);
     }
 
-    function decimals() external pure returns (uint8) {
+    function decimals() external view returns (uint8) {
         return IERC20Metadata(SBC).decimals();
     }
  }
