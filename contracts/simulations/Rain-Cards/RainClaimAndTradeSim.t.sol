692d4a15ec05a1119332e5bfa341fe613cd2f53e
Momin Ahmad <mominahmad93@gmail.com>
2024-08-19 17:44:06 -0400

    rain claim and trade

---
+++ contracts/simulations/Rain-Cards/RainClaimAndTradeSim.t.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.8.9;
+
+import "forge-std/Test.sol";
+import {ISpigotedLine} from "contracts/interfaces/ISpigotedLine.sol";
+import {ILineOfCredit} from "contracts/interfaces/ILineOfCredit.sol";
+import {ISpigot} from "contracts/interfaces/ISpigot.sol";
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+
+contract RainRe7Sim is Test {
+    ISpigotedLine spigotedLine;
+
+    // Fork Settings
+    uint256 constant FORK_BLOCK_NUMBER = 20_565_270; // Forking mainnet at block on 7/6/23 at 7 40 PM EST
+    uint256 ethMainnetFork;
+
+    address Rainline = 0x49845FCf0934A3114424fCf4A0ebF7F537d24dae;
+    address rainController = 0xE5D3d7da4b24bc9D2FDA0e206680CD8A00C0FeBD;
+    address spigot = 0x78176f8723F48a72FE9d2bE10D456529a77F7458;
+    address servicer = 0xeb0566b1EF38B95da2ed631eBB8114f3ac7b9a8a;
+
+    address claimToken = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address creditToken = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    bytes4 claimFunc = bytes4(0);
+    
+    
+    function setUp() public {
+        ethMainnetFork = vm.createFork(vm.envString("MAINNET_RPC_URL"), FORK_BLOCK_NUMBER);
+        vm.selectFork(ethMainnetFork);
+
+        spigotedLine = ISpigotedLine(Rainline);
+    }
+
+    function test_claim_and_trade_and_repay() public {
+
+        bytes memory tradeData = hex"d9627aa400000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000178000000000000000000000000000000000000000000000000000000000000017200000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48869584cd00000000000000000000000002a4d738d10c6560516fe4a4748d2f7a8bac24e700000000000000000000000000000000000000003adff5d9ea08dbb81bd2904b";
+
+        uint256 claimAmount = IERC20(claimToken).balanceOf(spigot);
+        uint256 balanceOfLineBefore = IERC20(creditToken).balanceOf(Rainline);
+
+        bytes memory claimFuncData = abi.encodeWithSelector(claimFunc);
+
+
+        ISpigot(spigot).claimRevenue(rainController, claimToken, claimFuncData);
+        
+        vm.prank(servicer);
+        spigotedLine.claimAndRepay(claimToken, tradeData);
+
+        uint256 balanceOfLineAfter = IERC20(creditToken).balanceOf(Rainline);
+
+        assertEq(IERC20(claimToken).balanceOf(spigot), 0);
+
+        assertGt(balanceOfLineAfter, balanceOfLineBefore);
+
+
+
+
+
+
+    }
+
+    function test_claim_and_trade() public {
+        vm.prank(servicer);
+    }
+}
